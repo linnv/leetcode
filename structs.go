5db4f280173a6085013bcbc8f98ca0c46bc2914b
JialinWu <win27v@gmail.com>
2017-03-16 10:40:52 +0800

    add method to get data of ListNode or TreeNode

--- structs.go
+++ structs.go
@@ -11,6 +11,65 @@ type ListNode struct {
 	Next *ListNode
 }
 
+func (ln *ListNode) ToArray() []int {
+	var data []int
+	head := ln
+	for head != nil {
+		data = append(data, head.Val)
+		head = head.Next
+
+	}
+	return data
+}
+
+func (tn *TreeNode) MidOrderData(treeData *[]int) []int {
+	if tn == nil {
+		return []int{}
+	}
+	tn.Left.MidOrderData(treeData)
+	*treeData = append(*treeData, tn.Val)
+	tn.Right.MidOrderData(treeData)
+	return *treeData
+}
+
+func (tn *TreeNode) PreOrderData(treeData *[]int) []int {
+	if tn == nil {
+		return []int{}
+	}
+	*treeData = append(*treeData, tn.Val)
+	tn.Left.PreOrderData(treeData)
+	tn.Right.PreOrderData(treeData)
+	return *treeData
+}
+
+func (tn *TreeNode) PosOrderData(treeData *[]int) []int {
+	if tn == nil {
+		return []int{}
+	}
+	tn.Left.PosOrderData(treeData)
+	tn.Right.PosOrderData(treeData)
+	*treeData = append(*treeData, tn.Val)
+	return *treeData
+}
+
+func (tn *TreeNode) LevelData() []int {
+	var data []int
+	treeQueue := []*TreeNode{tn}
+	var working *TreeNode
+	for len(treeQueue) > 0 {
+		working = treeQueue[0]
+		data = append(data, working.Val)
+		if working.Left != nil {
+			treeQueue = append(treeQueue, working.Left)
+		}
+		if working.Right != nil {
+			treeQueue = append(treeQueue, working.Right)
+		}
+		treeQueue = treeQueue[1:]
+	}
+	return data
+}
+
 func newListNode(val int) *ListNode {
 	return &ListNode{Val: val}
 }
